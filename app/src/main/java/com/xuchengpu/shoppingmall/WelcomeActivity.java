278786afca12fbdf99dd1624053b1978998e77e6
xuchengpu <1550540124@qq.com>
2017-02-22 10:54:40 +0800

    创建项目  欢迎界面跳转到主界面

---
+++ app/src/main/java/com/xuchengpu/shoppingmall/WelcomeActivity.java
@@ -0,0 +1,44 @@
+package com.xuchengpu.shoppingmall;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                toMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void toMainActivity() {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN) {
+            toMainActivity();
+            return  true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+}
