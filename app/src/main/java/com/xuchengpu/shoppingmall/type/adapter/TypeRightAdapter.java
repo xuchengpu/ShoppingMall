aad7e31c2fb37033b95c01699f80866003cb49c4
xuchengpu <1550540124@qq.com>
2017-03-03 18:40:38 +0800

    设置RecyclerView的适配器

--- app/src/main/java/com/xuchengpu/shoppingmall/type/adapter/TypeRightAdapter.java
+++ app/src/main/java/com/xuchengpu/shoppingmall/type/adapter/TypeRightAdapter.java
@@ -13,6 +13,7 @@ import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.xuchengpu.shoppingmall.R;
@@ -53,7 +54,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
 
     @Override
     public int getItemCount() {
-        return 1;
+        return 1 + child.size();
     }
 
     private int currenttype;
@@ -72,6 +73,8 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         if (viewType == HOT) {
             return new HotViewHolder(inflater.inflate(R.layout.item_hot_right, null));
+        } else if (viewType == CHILD) {
+            return new ChildViewHolder(inflater.inflate(R.layout.item_child_right, null));
         }
 
         return null;
@@ -82,9 +85,38 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
         if (getItemViewType(position) == HOT) {
             HotViewHolder hotViewHolder = (HotViewHolder) holder;
             hotViewHolder.setData();
-
+        } else if (getItemViewType(position) == CHILD) {
+            ChildViewHolder childViewHolder = (ChildViewHolder) holder;
+            int readPosition = position-1;
+            childViewHolder.setData(readPosition);
         }
 
+    }
+
+    class ChildViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.iv_ordinary_right)
+        ImageView ivOrdinaryRight;
+        @BindView(R.id.tv_ordinary_right)
+        TextView tvOrdinaryRight;
+        @BindView(R.id.ll_root)
+        LinearLayout llRoot;
+
+        public ChildViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void setData(final int readPosition) {
+            Glide.with(mContext).load(Constants.BASE_URL_IMAGE + child.get(readPosition).getPic()).placeholder(R.drawable.new_img_loading_2).into(ivOrdinaryRight);
+            tvOrdinaryRight.setText(child.get(readPosition).getName());
+            llRoot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "child.get(readPosition).getName():"+child.get(readPosition).getName(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
     }
 
     class HotViewHolder extends RecyclerView.ViewHolder {
@@ -93,29 +125,30 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
         @BindView(R.id.hsl_hot_right)
         HorizontalScrollView hslHotRight;
 
+
         public HotViewHolder(View itemView) {
             super(itemView);
-            ButterKnife.bind(this, itemView);
+            ButterKnife.bind(this,itemView);
         }
 
         public void setData() {
             for (int i = 0; i < hot_product_list.size(); i++) {
-                Log.e("tag","i=="+i);
+                Log.e("tag", "i==" + i);
                 LinearLayout linearLayout = new LinearLayout(mContext);
-                LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(-2,-2);
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(-2, -2);
                 linearLayout.setOrientation(LinearLayout.VERTICAL);
                 linearLayout.setGravity(Gravity.CENTER);
                 params.setMargins((DensityUtil.dip2px(mContext, 5)), 0, DensityUtil.dip2px(mContext, 5), DensityUtil.dip2px(mContext, 20));
 //                linearLayout.setLayoutParams(params);
 
-                ImageView imageView=new ImageView(mContext);
-                LinearLayout.LayoutParams ivParams=new LinearLayout.LayoutParams(DensityUtil.dip2px(mContext, 80), DensityUtil.dip2px(mContext, 80));
-                ivParams. setMargins(0, 0, 0, DensityUtil.dip2px(mContext, 10));
+                ImageView imageView = new ImageView(mContext);
+                LinearLayout.LayoutParams ivParams = new LinearLayout.LayoutParams(DensityUtil.dip2px(mContext, 80), DensityUtil.dip2px(mContext, 80));
+                ivParams.setMargins(0, 0, 0, DensityUtil.dip2px(mContext, 10));
                 Glide.with(mContext).load(Constants.BASE_URL_IMAGE + hot_product_list.get(i).getFigure()).into(imageView);
                 imageView.setLayoutParams(ivParams);
 
-                TextView textview=new TextView(mContext);
-                LinearLayout.LayoutParams tvParams=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                TextView textview = new TextView(mContext);
+                LinearLayout.LayoutParams tvParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 textview.setGravity(Gravity.CENTER);
                 textview.setTextColor(Color.parseColor("#ed3f3f"));
                 textview.setText("￥" + hot_product_list.get(i).getCover_price());
@@ -124,7 +157,7 @@ public class TypeRightAdapter extends RecyclerView.Adapter {
                 linearLayout.addView(imageView);
                 linearLayout.addView(textview);
 
-                llHotRight.addView(linearLayout,params);
+                llHotRight.addView(linearLayout, params);
 
                 final int finalI = i;
                 linearLayout.setOnClickListener(new View.OnClickListener() {
