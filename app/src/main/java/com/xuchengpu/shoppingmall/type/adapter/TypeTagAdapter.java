9ba4035bc7ecc4957985ebab98e0cdb4d7e70684
xuchengpu <1550540124@qq.com>
2017-03-03 19:53:04 +0800

    type类中的tag页面完成

---
+++ app/src/main/java/com/xuchengpu/shoppingmall/type/adapter/TypeTagAdapter.java
@@ -0,0 +1,79 @@
+package com.xuchengpu.shoppingmall.type.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xuchengpu.shoppingmall.R;
+import com.xuchengpu.shoppingmall.type.bean.TagBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by 许成谱 on 2017/3/3 18:51.
+ * qq:1550540124
+ * for:
+ */
+
+public class TypeTagAdapter extends BaseAdapter {
+    private final Context mContext;
+    private final List<TagBean.ResultBean> data;
+    private final int[] colors;
+
+    public TypeTagAdapter(Context mContext, List<TagBean.ResultBean> result, int[] colors) {
+        this.mContext = mContext;
+        this.data = result;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.item_tag, null);
+            viewHolder=new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder= (ViewHolder) convertView.getTag();
+        }
+        viewHolder.tvTag.setText(data.get(position).getName());
+        viewHolder.tvTag.setTextColor(colors[position%colors.length]);
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "name="+data.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return convertView;
+    }
+
+    static class ViewHolder {
+        @BindView(R.id.tv_tag)
+        TextView tvTag;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/xuchengpu/shoppingmall/type/fragment/TagFragment.java
+++ app/src/main/java/com/xuchengpu/shoppingmall/type/fragment/TagFragment.java
@@ -1,11 +1,24 @@
 package com.xuchengpu.shoppingmall.type.fragment;
 
 import android.graphics.Color;
-import android.view.Gravity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.GridView;
 
+import com.alibaba.fastjson.JSON;
+import com.xuchengpu.shoppingmall.R;
 import com.xuchengpu.shoppingmall.base.BaseFragment;
+import com.xuchengpu.shoppingmall.type.adapter.TypeTagAdapter;
+import com.xuchengpu.shoppingmall.type.bean.TagBean;
+import com.xuchengpu.shoppingmall.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
 
 /**
  * Created by 许成谱 on 2017/2/22 15:10.
@@ -14,17 +27,24 @@ import com.xuchengpu.shoppingmall.base.BaseFragment;
  */
 
 public class TagFragment extends BaseFragment {
-    private TextView textView;
+    @BindView(R.id.gv_tag)
+    GridView gvTag;
+
+    private int[] colors = {
+            Color.parseColor("#f0a420"), Color.parseColor("#4ba5e2"), Color.parseColor("#f0839a"),
+            Color.parseColor("#4ba5e2"), Color.parseColor("#f0839a"), Color.parseColor("#f0a420"),
+            Color.parseColor("#f0839a"), Color.parseColor("#f0a420"), Color.parseColor("#4ba5e2")
+    };
+
+
     /*
     初始化布局
     * */
     @Override
     public View initView() {
-        textView=new TextView(mContext);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(30);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_tag, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
     /*
     * 布局加载完后加载数据
@@ -33,6 +53,40 @@ public class TagFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        textView.setText("tag");
+        getDataFormNet(Constants.TAG_URL);
+
+    }
+
+    public void getDataFormNet(String url) {
+        OkHttpUtils.get()
+                .url(url)
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("tag", "联网请求失败==" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("tag", "联网请求成功==" + response);
+                        processData(response);
+                    }
+                });
+
+    }
+
+    private void processData(String response) {
+        TagBean tagBean= JSON.parseObject(response,TagBean.class);
+//        Log.e("tag","tagBean=="+tagBean.getResult().get(0).getName());
+        List<TagBean.ResultBean> result = tagBean.getResult();
+        if(result!=null&&result.size()>0) {
+            setTagAdapter(result);
+        }
+    }
+
+    private void setTagAdapter(List<TagBean.ResultBean> result) {
+        TypeTagAdapter adapter = new TypeTagAdapter(mContext,result,colors );
+        gvTag.setAdapter(adapter);
     }
 }
