3e66eca5626e2c3619d6f2a397cbb248d3ebb818
xuchengpu <1550540124@qq.com>
2017-03-05 21:36:32 +0800

    个人中心及顶部透明度变化完成

--- app/src/main/java/com/xuchengpu/shoppingmall/user/fragment/UserFragment.java
+++ app/src/main/java/com/xuchengpu/shoppingmall/user/fragment/UserFragment.java
@@ -1,11 +1,20 @@
 package com.xuchengpu.shoppingmall.user.fragment;
 
 import android.graphics.Color;
-import android.view.Gravity;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.hankkin.gradationscroll.GradationScrollView;
+import com.xuchengpu.shoppingmall.R;
 import com.xuchengpu.shoppingmall.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by 许成谱 on 2017/2/22 15:10.
@@ -13,18 +22,62 @@ import com.xuchengpu.shoppingmall.base.BaseFragment;
  * for:
  */
 
-public class UserFragment extends BaseFragment {
-    private TextView textView;
+public class UserFragment extends BaseFragment implements GradationScrollView.ScrollViewListener {
+    @BindView(R.id.ib_user_icon_avator)
+    ImageButton ibUserIconAvator;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.rl_header)
+    RelativeLayout rlHeader;
+    @BindView(R.id.tv_all_order)
+    TextView tvAllOrder;
+    @BindView(R.id.tv_user_pay)
+    TextView tvUserPay;
+    @BindView(R.id.tv_user_receive)
+    TextView tvUserReceive;
+    @BindView(R.id.tv_user_finish)
+    TextView tvUserFinish;
+    @BindView(R.id.tv_user_drawback)
+    TextView tvUserDrawback;
+    @BindView(R.id.tv_user_location)
+    TextView tvUserLocation;
+    @BindView(R.id.tv_user_collect)
+    TextView tvUserCollect;
+    @BindView(R.id.tv_user_coupon)
+    TextView tvUserCoupon;
+    @BindView(R.id.tv_user_score)
+    TextView tvUserScore;
+    @BindView(R.id.tv_user_prize)
+    TextView tvUserPrize;
+    @BindView(R.id.tv_user_ticket)
+    TextView tvUserTicket;
+    @BindView(R.id.tv_user_invitation)
+    TextView tvUserInvitation;
+    @BindView(R.id.tv_user_callcenter)
+    TextView tvUserCallcenter;
+    @BindView(R.id.tv_user_feedback)
+    TextView tvUserFeedback;
+    @BindView(R.id.ll_root)
+    LinearLayout llRoot;
+    @BindView(R.id.scrollview)
+    com.hankkin.gradationscroll.GradationScrollView scrollview;
+    @BindView(R.id.tv_usercenter)
+    TextView tvUsercenter;
+    @BindView(R.id.ib_user_setting)
+    ImageButton ibUserSetting;
+    @BindView(R.id.ib_user_message)
+    ImageButton ibUserMessage;
+
+    private int height;
+
     /*
-    初始化布局
-    * */
+        初始化布局
+        * */
     @Override
     public View initView() {
-        textView=new TextView(mContext);
-        textView.setTextColor(Color.RED);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(30);
-        return textView;
+        View view = View.inflate(mContext, R.layout.fragment_user, null);
+        ButterKnife.bind(this, view);
+        return view;
     }
     /*
     * 布局加载完后加载数据
@@ -33,6 +86,41 @@ public class UserFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        textView.setText("用户中心");
+        ViewTreeObserver observer = rlHeader.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //移除视图树的监听
+                rlHeader.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+
+                height=rlHeader.getHeight();
+
+                scrollview.setScrollViewListener(UserFragment.this);
+
+            }
+        });
+        tvUsercenter.setBackgroundColor(Color.argb(0,255,0,0));
+    }
+
+
+    @OnClick(R.id.tv_username)
+    public void onClick() {
+
+    }
+
+    @Override
+    public void onScrollChanged(GradationScrollView scrollView, int x, int y, int oldx, int oldy) {
+        if(y<=0) {
+            //设置标题的背景颜色  -透明
+            tvUsercenter.setBackgroundColor(Color.argb(0,255,0,0));
+        }else if(y>0&&y<=height){
+            //滑动的距离：height=实时透明度：透明度最大值
+            //实时透明度=透明度最大值*滑动的距离/height
+            int alpa=255*y/height;
+            tvUsercenter.setTextColor(Color.argb(alpa, 255, 255, 255));
+            tvUsercenter.setBackgroundColor(Color.argb(alpa,255,0,0));
+        }else{
+            tvUsercenter.setBackgroundColor(Color.argb(255,255,0,0));
+        }
     }
 }
